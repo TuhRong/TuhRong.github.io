ca6c22a9bde0d8dd6514cd90f78a76754fa92a69
Truong Le <truongle@uw.edu>
2023-02-06 19:17:49 -0800

    added chloropeth

--- COVID-Cases-Analysis/js/main.js
+++ COVID-Cases-Analysis/js/main.js
@@ -2,76 +2,79 @@ mapboxgl.accessToken =
     'pk.eyJ1IjoidHVocm9uZyIsImEiOiJjbGRzOWcxZm8wc216M29tczRubHoyeXY0In0.MZeb3zCRCCakYDKq4yDhnw';
 let map = new mapboxgl.Map({
     container: 'map', // container ID
+    projection: 'albers',
     style: 'mapbox://styles/mapbox/dark-v10',
     zoom: 5, // starting zoom
-    minZoom: 4, // minimum zoom level of the map
     center: [-122.335167, 47.608013] // starting center
 });
 const grades = [4, 5, 6],
     colors = ['rgb(208,209,230)', 'rgb(103,169,207)', 'rgb(1,108,89)'],
     radii = [5, 15, 20];
-//load data to the map as new layers.
-//map.on('load', function loadingData() {
-map.on('load', () => { //simplifying the function statement: arrow with brackets to define a function
-    // when loading a geojson, there are two steps
-    // add a source of the data and then add the layer out of the source
-    map.addSource('us-covid-2020-counts', {
-        type: 'json',
-        data: 'assets/us-covid-2020-counts.json'
-    });
-    map.addLayer({
-            'id': 'covid-count',
-            'type': 'circle',
-            'source': 'us-covid-2020-counts',
-            'paint': {
-                // increase the radii of the circle as the zoom level and dbh value increases
-                'circle-radius': {
-                    'property': 'mag',
-                    'stops': [
-                        [grades[0], radii[0]],
-                        [grades[1], radii[1]],
-                        [grades[2], radii[2]]
-                    ]
-                },
-                'circle-color': {
-                    'property': 'mag',
-                    'stops': [
-                        [grades[0], colors[0]],
-                        [grades[1], colors[1]],
-                        [grades[2], colors[2]]
-                    ]
-                },
-                'circle-stroke-color': 'white',
-                'circle-stroke-width': 1,
-                'circle-opacity': 0.6
-            }
-        }
-    );
+        //load data to the map as new layers.
+        //map.on('load', function loadingData() {
+            map.on('load', () => { //simplifying the function statement: arrow with brackets to define a function
+                // when loading a geojson, there are two steps
+                // add a source of the data and then add the layer out of the source
+                map.addSource('us-covid-2020-counts', {
+                    type: 'geojson',
+                    data: 'assets/us-covid-2020-counts.json'
+                });
+                map.addLayer({
+                    'id': 'covid-counts',
+                    'type': 'circle',
+                    'source': 'us-covid-2020-counts',
+                    'minzoom': 3,
+                    'paint': {
+                        // increase the radii of the circle as mag value increases
+                        'circle-radius': {
+                            'property': 'cases',
+                            'stops': [
+                                [grades[0], radii[0]],
+                                [grades[1], radii[1]],
+                                [grades[2], radii[2]]
+                            ]
+                        },
+                        // increase the radii of the circle as COVID cases increases
+                        'circle-color': {
+                            'property': 'cases',
+                            'stops': [
+                                [grades[0], colors[0]],
+                                [grades[1], colors[1]],
+                                [grades[2], colors[2]]
+                            ]
+                        },
+                        'circle-stroke-color': 'white',
+                        'circle-stroke-width': 1,
+                        'circle-opacity': 0.6
+                    }
+                });
     // click on tree to view magnitude in a popup
-    map.on('click', 'covid-count', (event) => {
-        new mapboxgl.Popup()
-            .setLngLat(event.features[0].geometry.coordinates)
-            .setHTML(`<strong>Magnitude:</strong> ${event.features[0].properties.mag}`)
-            .addTo(map);
-    });
-});
-// create legend
-const legend = document.getElementById('legend');
-//set up legend grades and labels
-var labels = ['<strong>Magnitude</strong>'],
-    vbreak;
-//iterate through grades and create a scaled circle and label for each
-for (var i = 0; i < grades.length; i++) {
-    vbreak = grades[i];
-    // you need to manually adjust the radius of each dot on the legend 
-    // in order to make sure the legend can be properly referred to the dot on the map.
-    dot_radii = 2 * radii[i];
-    labels.push(
-        '<p class="break"><i class="dot" style="background:' + colors[i] + '; width: ' + dot_radii +
-        'px; height: ' +
-        dot_radii + 'px; "></i> <span class="dot-label" style="top: ' + dot_radii / 2 + 'px;">' + vbreak +
-        '</span></p>');
-}
-const source =
-    '<p style="text-align: right; font-size:10pt">Source: <a href="https://earthquake.usgs.gov/earthquakes/">USGS</a></p>';
-legend.innerHTML = labels.join('') + source;
+    map.on('click', 'covid-counts', (event) => {
+                    new mapboxgl.Popup()
+                        .setLngLat(event.features[0].geometry.coordinates)
+                        .setHTML(`<strong>Cases:</strong> ${event.features[0].properties.cases}`)
+                        .addTo(map);
+                });
+            });
+            // create legend
+            const legend = document.getElementById('legend');
+            //set up legend grades and labels
+            var labels = ['<strong>Cases</strong>'],
+                vbreak;
+            //iterate through grades and create a scaled circle and label for each
+            for (var i = 0; i < grades.length; i++) {
+                vbreak = grades[i];
+                // you need to manually adjust the radius of each dot on the legend 
+                // in order to make sure the legend can be properly referred to the dot on the map.
+                dot_radii = 2 * radii[i];
+                labels.push(
+                    '<p class="break"><i class="dot" style="background:' + colors[i] + '; width: ' + dot_radii +
+                    'px; height: ' +
+                    dot_radii + 'px; "></i> <span class="dot-label" style="top: ' + dot_radii / 2 + 'px;">' + vbreak +
+                    '</span></p>');
+            }
+            // add the data source
+            const source =
+                '<p style="text-align: right; font-size:10pt">Source: <a href="https://earthquake.usgs.gov/earthquakes/">USGS</a></p>';
+            // combine all the html codes.
+            legend.innerHTML = labels.join('') + source;
